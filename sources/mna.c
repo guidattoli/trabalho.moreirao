9b3f4baef82c7ebd91f2f771b3cc3ed8c3dfac84
rodrigofructuoso <rofructuoso@hotmail.com>
2013-10-27 20:49:30 -0200

    Debugging 1.1
    Adicionando prints para localizar erro!

--- sources/mna.c
+++ sources/mna.c
@@ -29,20 +29,16 @@ int main(int argc, char *argv[])
   char variaveis[NUMERO_MAX_COMPONENTES+1];
   
   quantidadeNos=quantidadeElementos=0;
-
-	leitura = fopen (argv[1], "r");
-	if(leitura == NULL)
-	return ERRO_ABRIR_ARQUIVO;
-	  
- /* if(!abreArquivo(argv[1],leitura))
-    exit(ERRO_ABRIR_ARQUIVO); */
+    
+  if(abreArquivo(argv[1],leitura))
+  exit(ERRO_ABRIR_ARQUIVO);
   
-  printf("passei aqui 1 \n");
+  printf("passei aqui 1!");
   
   geraVetorElementos(leitura,&vetorComponentes,quantidadeNos,quantidadeElementos, &informacoes);
   tamanhoMatriz = quantidadeNos + quantidadeElementos + 1;
   
-  printf("passei aqui 2 \n");
+  printf("passei aqui 2!");
   
   for(contador=0;contador<quantidadeElementos;contador++)
   printf("Nome :%s\nValor : %lf\n",vetorComponentes[contador].nome,vetorComponentes[contador].valor);
